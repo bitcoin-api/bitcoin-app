1ad60702ac0aff6a6d3cfa99c698ff90028bec3a
Michael Stecky-Efantis <m.stecky.efantis@gmail.com>
2020-05-06 21:50:35 -0400

    style update

--- Main/getAppElements/ActionBox/NotLoggedIn/index.js
+++ Main/getAppElements/ActionBox/NotLoggedIn/index.js
@@ -8,7 +8,7 @@ import {
 import getPastedTokenData from './getPastedTokenData';
 import AsyncStorage from '@react-native-community/async-storage';
 import { getState, setState } from '../../../../reduxX';
-import { colours } from '../../../constants';
+import { fonts } from '../../../constants';
 
 
 const getStyles = () => {
@@ -42,6 +42,7 @@ const getStyles = () => {
         buttonText: {
 
             color: mainStyleObject.color,
+            fontFamily: fonts.merriweather.regular
         }
     });
 };
