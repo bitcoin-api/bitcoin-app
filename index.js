8aa1845fdd72553374b79b89f4eda93dc6177a69
Michael Stecky-Efantis <m.stecky.efantis@gmail.com>
2020-05-06 21:58:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,37 +6,40 @@ import {
 import Main from './Main';
 import { getState, setUpReduxX } from './reduxX';
 
-const getStyles = ({
-    backgroundColor,
-}) => StyleSheet.create({
-    outerContainer: {
-        backgroundColor,
-        // width: '100%',
-        // height: '100%',
-        flex: 1,
-        
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
+const getStyles = () => {
+
+    const {
+
+        backgroundColor
+
+    } = getState( 'mainStyleObject' );
+    
+    return StyleSheet.create({
+        outerContainer: {
+            backgroundColor,
+            // width: '100%',
+            // height: '100%',
+            flex: 1,
+            
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    })
+};
 
 
 export default () => {
 
     setUpReduxX( useState );
 
-    const mainStyleObject = getState( 'mainStyleObject' );
+    const styles = getStyles()
 
     return e(
         View,
         {
-            style: getStyles({
-
-                backgroundColor: mainStyleObject.backgroundColor
-
-            }).outerContainer,
+            style: styles.outerContainer,
         },
         e( Main )
     );
